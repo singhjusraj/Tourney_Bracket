0a2184a3e35af0615c6f672d18c6356d2e08b06b
Sukhdeep Singh <sukhdeepsaini12@gmail.com>
2014-01-15 12:30:05 -0500

    Update update_tournament.php

--- update_tournament.php
+++ update_tournament.php
@@ -22,7 +22,7 @@ File Description: This page Takes the input from the Modify.php page and makes t
 				header('Location:index.php');
 			} else {
 
-				$conn = mysqli_connect('webdesign4', 'db200245935', '37949', 'db200245935') or die('Error connecting to MySQL server');
+				$conn = mysqli_connect('webdesign4', 'dbxxxxxxxxx', 'xxxxx', 'dbxxxxxxxxx') or die('Error connecting to MySQL server');
 				
 				$winner1 = $_POST['winner1'];
 				$winner2 = $_POST['winner2'];
@@ -30,7 +30,7 @@ File Description: This page Takes the input from the Modify.php page and makes t
 				$winner4 = $_POST['winner4'];				
 				$id = $_POST['id'];	
 
-				$conn = mysqli_connect('webdesign4', 'db200245935', '37949', 'db200245935') or die('Error connecting to MySQL server');
+				$conn = mysqli_connect('webdesign4', 'dbxxxxxxxxx', 'xxxxx', 'dbxxxxxxxxx') or die('Error connecting to MySQL server');
 
 				$query = 'UPDATE tournaments SET winner1 = "'.$winner1.'", winner2 = "'.$winner2.'", winner3 = "'.$winner3.'", winner4 = "'.$winner4.'" WHERE id = "'.$id.'"';
 				mysqli_query($conn, $query) or die('Error querying database.');
